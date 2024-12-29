15375071a5c070054dcde32069d53bd7fa288853
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-12-30 02:15:13 +0900

    9996sol

--- 9996.cpp
+++ 9996.cpp
@@ -1,38 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int N;
-
-
+#include<bits/stdc++.h> 
+using namespace std;   
+int n; 
+string s, ori_s, pre, suf; 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> N;
-    string file[N];
-    string pattern;
-    cin >> pattern;
-    string first, last;
-    int i;
-    for(i = 0; pattern[i] == '*'; i++){
-        first += pattern[i];
-    }
-    int tmp = i;
-    for(tmp; tmp < pattern.size()-1; tmp++)
-    {
-        last += pattern[tmp];
-    }
-    for(int i = 0 ; i < N; i++){
-        cin >> file[i];
-
-    }
-
-    for(int j = 0; j < N; j++){
-        if(*file[j].begin() == first && file[j].back() == last)
-        {
-            cout << "DA" << "\n";
-        }
-        else cout << "NE" << "\n";
-    }
-
+    cin >> n;
+    cin >> ori_s;  
+    int pos = ori_s.find('*');  
+    pre = ori_s.substr(0, pos); 
+    suf = ori_s.substr(pos + 1); 
+    for(int i = 0; i < n; i++){
+        cin >> s; 
+        if(pre.size() + suf.size() > s.size()){
+            cout << "NE\n";
+        }else{
+            if(pre == s.substr(0, pre.size()) && suf == s.substr(s.size() - suf.size())) cout << "DA\n";
+            else cout <<"NE\n";  
+        } 
+    } 
+    return 0;
 }
